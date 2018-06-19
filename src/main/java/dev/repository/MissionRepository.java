be54537b9a7387b11f927a1f65ee46d5151d72ee
Sebastien Vignoud <sebastien.vignoud@gmail.com>
2018-06-19 17:34:22 +0200

    resolution du probeme d'enregistrement multiple en base du jeu de donnees test

--- src/main/java/dev/repository/MissionRepository.java
+++ src/main/java/dev/repository/MissionRepository.java
@@ -3,5 +3,8 @@ package dev.repository;
 import dev.model.Mission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MissionRepository extends JpaRepository<Mission, Integer> {
+    List<Mission> findByNatureName(String name);
 }
