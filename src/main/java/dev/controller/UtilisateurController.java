b724d21e9fda78c93d05eb25fffa59a8af7a8436
Sebastien Vignoud <sebastien.vignoud@gmail.com>
2018-06-21 16:48:18 +0200

    implementation GET pour les utilisateurs

---
+++ src/main/java/dev/controller/UtilisateurController.java
@@ -0,0 +1,30 @@
+package dev.controller;
+
+import dev.exception.ItemNotFoundException;
+import dev.model.Utilisateur;
+import dev.repository.UtilisateurRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/utilisateurs")
+@CrossOrigin
+public class UtilisateurController {
+
+    @Autowired
+    private UtilisateurRepository utilisateurRepository;
+
+    @GetMapping
+    public List<Utilisateur> getUtilisateurs(){return this.utilisateurRepository.findAll();}
+
+    @GetMapping("/{id}")
+    public Utilisateur getUtilisateurById(@PathVariable Integer id) throws ItemNotFoundException {
+        if (this.utilisateurRepository.findOne(id) == null){
+            throw new ItemNotFoundException();
+        }
+        return this.utilisateurRepository.findOne(id);
+    }
+    
+}
